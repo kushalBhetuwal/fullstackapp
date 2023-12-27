9b40124e16257c8fdc13808d269873c51d09c79c
kushal <kushalbhetuwal@gmail.com>
2023-12-27 23:33:11 +1100

    added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -27,16 +27,42 @@ const Home = () => {
             }
         }
         fetchData();
-    }, [])
+    }, []);
+    const createUser= async(e:React.FormEvent<HTMLFormElement>)=>{
+      e.preventDefault();
+      
+    }
+    const deleteUser = async(id:number)=>{
+        try{
+          const response = await fetch(`${apiurl}/users/${id}`, {
+            method: 'DELETE',
+          })
+
+         if(!response.ok){
+          throw new Error('response not okay')
+         }
+         const data  = await response.json()
+         console.log(data);
+         setUsers(users.filter(user=>user.id!==id))
+        }
+        catch(error){
+          console.log(error);
+        }
+    }
   return (
     <div className="flex justify-center items-center flex-col">
         <h1 className="text-[3rem]">User Management</h1>
+        <form className="w-[25%] h-full bg-sky-400 flex flex-col items-center justify-center p-4 mt-4 rounded-lg" onSubmit={createUser}>
+        <input type="text" placeholder="Name" value={newUser.name} className="p-3 w-full rounded-[12px] focus:outline-none" onChange={(e)=>setNewUser({...newUser, name:e.target.value})} />
+        <input type="text" placeholder="Email" value={newUser.email} className="p-3 w-full mt-4 rounded-[12px] focus:outline-none" onChange={(e)=>setNewUser({...newUser, email:e.target.value})}/>
+        <button type="submit" className="bg-sky-700 hover:text-black text-white w-full p-2 h-[40%] mt-8 border-none rounded-[12px] hover:bg-yellow-300">Add User</button>
+      </form>
         {
             users.map((user)=>{
                 return (
                     <div key={user.id} className='my-2 flex justify-between w-[400px] bg-green-300 p-2 rounded-lg'>
                     <CardComponent  card={user}/>
-                    <button className="bg-green-400 p-2 h-[40%] mt-8 border-none rounded-[12px] hoverbutton" onClick={()=>{}}>Delete User</button>
+                    <button className="bg-green-400 p-2 h-[40%] mt-8 border-none rounded-[12px] hoverbutton" onClick={()=>{deleteUser(user.id)}}>Delete User</button>
                     </div>
                    
                 )
